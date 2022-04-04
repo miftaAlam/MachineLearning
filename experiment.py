a3dc5d32e361ef32c1f8697b4d566cd7496824da
Matt Roberts <matthew.roberts@mq.edu.au>
2022-04-04 07:01:59 +0100

    mobile net on images looking good

--- experiment.py
+++ experiment.py
@@ -1,55 +1,29 @@
-import tflite_runtime.interpreter as tflite
 import numpy as np
 from PIL import Image
 import time
 
-models = [("MobileNet V1 224","models/mobilenet_v1_1.0_224_quant/mobilenet_v1_1.0_224_quant.tflite"),
-          ("MobileNet V1 128","models/mobilenet_v1_0.25_128_quant/mobilenet_v1_0.25_128_quant.tflite")
-         ]
+def run_prediction(model, image)
 
-print("Which model would you like to run?")
-for i, mod in enumerate(models):
-  print(f"({i}) {mod[0]}")
-index = int(input())
+  for img_path in :
+    print(f"=== {img_path} ===")
+    img = Image.open(f"{img_path}").convert('RGB').resize((width, height))
 
-interpreter = tflite.Interpreter(model_path=models[index][1])
-interpreter.allocate_tensors()
+    # we need another dimension
+    input_data = np.expand_dims(img, axis=0)
 
-inputs = interpreter.get_input_details();
-outputs = interpreter.get_output_details();
-width  = inputs[0]["shape"][2]
-height = inputs[0]["shape"][1]
-dtype = inputs[0]["dtype"]
+    # lets get to it
+    print("# Interpreter:")
+    print("  * starting")
+    interpreter.set_tensor(inputs[0]["index"], input_data)
 
-print("# inputs")
-print(f"  * height = {width}")
-print(f"  * width  = {height}")
-print(f"  * type   = {dtype}")
+    start_time = time.time()
+    interpreter.invoke()
+    print(f"  * Interpreter done after {time.time() - start_time}")
 
-lables=[]
-with open("models/mobilenet_labels.txt", "r") as f:
-    labels = [line.strip() for line in f.readlines()]
+    output_data = interpreter.get_tensor(outputs[0]["index"]).squeeze()
 
-for img_path in ["images/224x224/mug.jpg", "images/224x224/paper_cup.jpg", "images/224x224/can.jpg", "images/room.jpg", "images/239x215/cat.jpg", "images/128x128/example.jpg", "images/128x128/mug.jpg", "images/room.jpg"]:
-  print(f"=== {img_path} ===")
-  img = Image.open(f"{img_path}").convert('RGB').resize((width, height))
-
-  # we need another dimension
-  input_data = np.expand_dims(img, axis=0)
-
-  # lets get to it
-  print("# Interpreter:")
-  print("  * starting")
-  interpreter.set_tensor(inputs[0]["index"], input_data)
-
-  start_time = time.time()
-  interpreter.invoke()
-  print(f"  * Interpreter done after {time.time() - start_time}")
-
-  output_data = interpreter.get_tensor(outputs[0]["index"]).squeeze()
-
-  ordered_indexes = np.flip(output_data.argsort())
-  print("  * predictions")
-  for i in ordered_indexes[:5]:
-      print(f"    * %0.3f prediction of {labels[i]}" % output_data[i])
+    ordered_indexes = np.flip(output_data.argsort())
+    print("  * predictions")
+    for i in ordered_indexes[:5]:
+        print(f"    * %0.3f prediction of {labels[i]}" % output_data[i])
 
--- predict.py
+++ predict.py
@@ -1,4 +1,9 @@
 import argparse
+import tflite_runtime.interpreter as tflite
+import numpy as np
+from PIL import Image
+import time
+import glob
 
 argparser = argparse.ArgumentParser(description="Run a Model on a set of images or a camera feed and generate predictions.")
 argparser.add_argument("--model", metavar="M", type=int, help="the index of the model to use.")
@@ -9,8 +14,13 @@ models = [("MobileNet V1 224","models/mobilenet_v1_1.0_224_quant/mobilenet_v1_1.
           ("MobileNet V1 128","models/mobilenet_v1_0.25_128_quant/mobilenet_v1_0.25_128_quant.tflite")
          ]
 
-sources = ["Example Images",
-           "Camera"
+sources = [("Example Images",["images/224x224/*",
+                              "images/room.jpg", 
+                              "images/239x215/*", 
+                              "images/128x128/*",
+                              "images/imagenet_examples/*"
+                              ]),
+           ("Camera","")
           ]
 
 if args.model is None:
@@ -24,10 +34,45 @@ else:
 if args.source is None:
   print("Which source would you like predict from?")
   for i, src in enumerate(sources):
-    print(f"({i}) {src}")
+    print(f"({i}) {src[0]}")
   src_i = int(input())
 else:
   src_i = args.source
 
-print(f"Predicting with model:  {models[model_i][0]}")
-print(f"Predicting from source: {sources[src_i]}")
+interpreter = tflite.Interpreter(models[model_i][1])
+interpreter.allocate_tensors()
+
+inputs = interpreter.get_input_details()[0];
+outputs = interpreter.get_output_details()[0];
+width  = inputs["shape"][2]
+height = inputs["shape"][1]
+dtype = inputs["dtype"]
+scale, zero = outputs['quantization']
+
+print(f"Predicting with model:  {models[model_i][0]} ({width}x{height}) {dtype}")
+
+lables=[]
+with open("models/mobilenet_labels.txt", "r") as f:
+    labels = [line.strip() for line in f.readlines()]
+
+print(f"Predicting from source: {sources[src_i][0]}")
+
+for img_glob in sources[src_i][1]:
+  for img_path in glob.glob(img_glob):
+    img = Image.open(img_path).convert('RGB').resize((width, height))
+
+    # we need another dimension
+    input_data = np.expand_dims(img, axis=0)
+
+    # lets get to it
+    interpreter.set_tensor(inputs["index"], input_data)
+
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(outputs["index"]).squeeze()
+    
+    output_data = (scale*100) * (output_data - zero)
+
+    ordered_indexes = np.flip(output_data.argsort())
+    best_index = ordered_indexes[0]
+    print(f"  * {img_path} = {labels[best_index]} %0.0f%%" % output_data[best_index])
